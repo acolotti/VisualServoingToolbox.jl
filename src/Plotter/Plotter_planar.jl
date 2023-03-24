dd96365543e05c2f6f6eb377333f7db5ea2a7ff4
Alessandro Colotti <alessandro.colotti@ls2n.fr>
2023-03-24 14:01:38 +0100

    Switched planar systems plots to GLMakie

--- src/Plotter/Plotter_planar.jl
+++ src/Plotter/Plotter_planar.jl
@@ -1,52 +1,64 @@
-function plotCameraTrajectories(traj::Vector{VS.VSTrajectory{VS.planar}}, P::AbstractMatrix = [0 0]; scale::Real=0.15)
-
-    (xTraj,zTraj,θTraj,xBegin,zBegin,θBegin,xFinal,zFinal,θFinal) = Uti.vectorizeTrajectories(traj);
-
-    plt = Plots.plot(xTraj,zTraj,linecolor=:blue,aspect_ratio = 1,legend=:none);
-    Plots.plot!(plt,xTraj + map(x->map(sin,x),θTraj),zTraj + map(x->map(cos,x),θTraj),linecolor=:red);
+function plotCameraTrajectories(traj::Vector{VS.VSTrajectory{VS.planar}}, P::AbstractMatrix = [0 0]; equal_axes::Bool = true, point_size::Real = 10, plot_leading_vector::Bool = false, scale::Real=0.15)
+    fig = GLM.Figure();
+    ax = GLM.Axis(fig[1,1], title = "", aspect = GLM.DataAspect());
 
     if P != [0 0]
-        Plots.plot!(plt,P[1,:], P[2,:], m=(10,:o), markercolor=:black,linetype=:scatter);
+        GLM.scatter!(ax,P[1,:], P[2,:], markersize=point_size, color = :black); # plots tracking points
     end
 
-    Plots.plot!(plt,getPlanarShapes(xBegin,zBegin,θBegin,scale),seriescolor=:purple,opacity=.1);
-    Plots.plot!(plt,getPlanarShapes(xFinal,zFinal,θFinal,scale),seriescolor=:blue,opacity=.2);
+    for t in traj
+        GLM.lines!(ax,map(e->GLM.Point2f(e[1:2]),t.u),color=:blue);
+        plot_leading_vector && GLM.lines!(ax,map(e->GLM.Point2f(e[1:2]+[sin(x),cos(x)]),t.u),color=:red);
+        GLM.poly!(ax, getPlanarShapes(t.u[1],scale), color = GLM.RGBA{Float32}(0, 0, 1, 0.1));
+        GLM.poly!(ax, getPlanarShapes(t.u[end],scale), color = GLM.RGBA{Float32}(160/255, 32/255, 240/255, 0.2));
+    end
 
-    return plt;
+    if equal_axes
+        # what follows is a hack to give the plot a "square" shape
+        GLM.reset_limits!(ax); # updates axes limits to fit the content of ax
+        L = argmax(ax.finallimits[].widths); 
+        M = ax.finallimits[].widths[L]/2;
+
+        limits = Vector{Float64}(undef,4);
+
+        for i = 1:2
+            m = ax.finallimits[].origin[i] + ax.finallimits[].widths[i]/2;
+            limits[2*i-1:2*i] = [m-M, m+M];
+        end
+
+        GLM.limits!(ax,limits...);
+    end
+
+    return fig;
 end
 
-function plotCameraTrajectoriesIn3D(traj::Vector{VS.VSTrajectory{VS.planar}}, P::AbstractMatrix = [0 0])
-
-    (xTraj,zTraj,θTraj,xBegin,zBegin,θBegin,xFinal,zFinal,θFinal) = Uti.vectorizeTrajectories(traj);
-
-    # if you want to choose your own color palette (i.e. you have a vector cols of 
-    # colors) you can add the option palette=palette(cols)
-    # https://docs.juliaplots.org/latest/generated/colorschemes/
-
-    plt = Plots.plot3d(xTraj,zTraj,θTraj,legend=:none);
+function plotCameraTrajectoriesIn3D(traj::Vector{VS.VSTrajectory{VS.planar}}, P::AbstractMatrix = [0 0]; point_size::Real = 10, endpoints_size::Real = 25)
+    fig = GLM.Figure();
+    ax = GLM.Axis3(fig[1,1], title = "", aspect = :data);
 
     if P != [0 0]
-        Plots.plot3d!(plt,P[1,:], P[2,:], zeros(size(P,2)), m=(10,:o), markercolor=:black,linetype=:scatter);
+        GLM.scatter!(ax, P[1,:], P[2,:], zeros(size(P,2)), markersize = point_size, color = :black); # plots tracking points
     end
-    Plots.plot3d!(plt,xBegin,zBegin,θBegin,m=(10,:hexagon),markercolor=:green,linetype=:scatter,opacity=.2);
-    Plots.plot3d!(plt,xFinal,zFinal,θFinal,m=(10,:xcross),markercolor=:red,linetype=:scatter,opacity=.4);
 
-    return plt;
+    for t in traj
+        GLM.lines!(ax, map(GLM.Point3f,t.u));
+        GLM.scatter!(ax, map(GLM.Point3f,[t.u[1],t.u[end]]), color = [:green,:red], markersize = endpoints_size, marker = [:hexagon,'x']);
+    end
 
+    return fig;
 end
 
-function getPlanarShapes(x::Vector{<:Real},z::Vector{<:Real},θ::Vector{<:Real},mSize::Real = 0.15)
-    n = length(x);
-    xS = Array{Float64}(undef,4*n);
-    zS = Array{Float64}(undef,4*n);
+function getPlanarShapes(state::Vector{<:Real},size::Real = 0.15)
+    x, y, θ = state[1:3];
 
-    R(θ) = [cos(θ) sin(θ); -sin(θ) cos(θ)];
+    basic_shape = size*[GLM.Point2f(0,-1), GLM.Point2f(0.75,1), GLM.Point2f(-0.75,1)]; # vertices of a triangle which is "looking up"
+    R = [cos(θ) sin(θ); -sin(θ) cos(θ)]; # matrix that allows to rotate the triangle in the same direction of the camera
+    
+    camera_shape = Vector{GLM.Point2f}(undef,3);
 
-    for i = 1:n
-        currentTriangle = mSize*R(θ[i])*[-1 1 0; -1 -1 1] + [x[i];z[i]]*[1 1 1];
-        xS[4*i-3:4*i] = [currentTriangle[1,:]..., NaN];
-        zS[4*i-3:4*i] = [currentTriangle[2,:]..., NaN];
+    for i = 1:3
+        camera_shape[i] = R*basic_shape[i] + [x;y]; # roto-translation of each vertex to the correct position
     end
 
-    return Plots.Shape(xS,zS);
+    return camera_shape;
 end
