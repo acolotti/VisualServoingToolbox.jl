9342cf0a0676ba28c681d9116493d5320c4a521d
Alessandro <alessandro.colotti@inria.fr>
2024-08-01 11:08:27 +0200

    Corrected conflicting 'simulate' definitions.

--- src/Simulator/Simulator.jl
+++ src/Simulator/Simulator.jl
@@ -8,7 +8,7 @@ import ..VSSystems as VS
 export simulate
 
 """
-    simulate(f, x0, p, t; controller=VS.transposeController, solver=TRBDF2, quiet=false)
+    simulate(f, x0, p, t; solver=TRBDF2, quiet=false)
 
 Simulate the visual servoing system defined by `f`.
 
@@ -16,6 +16,7 @@ Simulate the visual servoing system defined by `f`.
 - `f::Function`: system's vector field.
 - `x0::Vector{Vector{<:Real}}`: Vector of initial positions.
 - `p`: system's parameters, depend on the chosen vector field.
+- `sd::AbstractArray{<:Real}`: (f x N) matrix, where f is the features' dimension. Each column is a tracking point's wanted feature.
 - `t::Real`: simulation time.
 - `solver=DE.TRBDF2`: ODE solver. Can be choosen from: https://diffeq.sciml.ai/stable/solvers/ode_solve/
 - `quiet::Bool=false`: option to suppress output.
@@ -28,25 +29,30 @@ function simulate(f::Function, x0::Vector{Vector{T}} where T<:Real, p, t::Real; 
 
     return simulate(f,M0,p,t,solver=solver,quiet=quiet,step_size=step_size,terminal_distance=terminal_distance,terminal_state=terminal_state,terminal_threshold=terminal_threshold);
 end
-
 """
-    simulate(f, x0, p, t; controller=VS.transposeController, solver=TRBDF2, quiet=false)
+    simulate(f, x0, p, t; solver=TRBDF2, quiet=false)
 
 Simulate the visual servoing system defined by `f`.
 
 # Arguments
 - `f::Function`: system's vector field.
 - `x0::AbstractArray{<:Real}`: (n x s) matrix, where n is the system's dimension. Each column is a different initial position.
-- `p::Vector{Any}`: system's parameters, depend on the chosen vector field.
+- `p`: system's parameters, depend on the chosen vector field.
+- `sd::AbstractArray{<:Real}`: (f x N) matrix, where f is the features' dimension. Each column is a tracking point's wanted feature.
 - `t::Real`: simulation time.
 - `solver=DE.TRBDF2`: ODE solver. Can be choosen from: https://diffeq.sciml.ai/stable/solvers/ode_solve/
 - `quiet::Bool=false`: option to suppress output.
 """
-function simulate(f::Function, x0::AbstractArray{<:Real}, p::Vector{Any}, t::Real; solver=DE.TRBDF2, quiet::Bool=false, 
-    step_size::Union{Real,Nothing} = nothing, terminal_state::Union{Vector{T} where T<:Real, Nothing} = nothing, terminal_threshold::Real = 1e-6)
-    
-    P = p[1];
-    sd = p[2];
+function simulate(f::Function, x0::AbstractArray{<:Real}, p, t::Real; solver=DE.TRBDF2, quiet::Bool=false, 
+    step_size::Union{Real,Nothing} = nothing, terminal_distance::Function = VS.squaredDistance, 
+    terminal_state::Union{Vector{T} where T<:Real, Vector{Vector{T}} where T<:Real, Nothing} = nothing, terminal_threshold::Real = 1e-6)
+
+    if !isa(p,NamedTuple)
+        @error "Passing the simulation's parameters as a vector is deprecated, use named tuples instead.";
+    end
+
+    P = p.P;
+    sd = p.sd;
     
     if size(P,2) != size(sd,2)
         if size(P,2) == size(sd,1)
@@ -62,12 +68,14 @@ function simulate(f::Function, x0::AbstractArray{<:Real}, p::Vector{Any}, t::Rea
 
     if quiet
         for i = 1:s
-            trajectories[i] = singleSimulation(f,x0[:,i],p,t,solver,step_size=step_size,terminal_state=terminal_state,terminal_threshold=terminal_threshold);
+            trajectories[i] = singleSimulation(f,x0[:,i],p,t,solver,step_size=step_size,
+                terminal_distance = terminal_distance, terminal_state=terminal_state,terminal_threshold=terminal_threshold);
         end
     else
         for i = 1:s
             println("Simulation $i started..");
-            trajectories[i] = singleSimulation(f,x0[:,i],p,t,solver,step_size=step_size,terminal_state=terminal_state,terminal_threshold=terminal_threshold);
+            trajectories[i] = singleSimulation(f,x0[:,i],p,t,solver,step_size=step_size,
+                terminal_distance = terminal_distance, terminal_state=terminal_state,terminal_threshold=terminal_threshold);
             println("Finished.");
         end
     end
@@ -76,7 +84,7 @@ function simulate(f::Function, x0::AbstractArray{<:Real}, p::Vector{Any}, t::Rea
 end
 
 """
-    simulate(f, x0, P, sd, t; controller=VS.transposeController, solver=TRBDF2, quiet=false)
+    simulate(f, x0, P, sd, t; solver=TRBDF2, quiet=false)
 
 Simulate the visual servoing system defined by `f`, function kept for back-compatibility.
 
@@ -91,27 +99,26 @@ Simulate the visual servoing system defined by `f`, function kept for back-compa
 - `quiet::Bool=false`: option to suppress output.
 """
 function simulate(f::Function, x0::AbstractArray{<:Real}, P::AbstractMatrix{<:Real}, sd::AbstractArray{<:Real}, t::Real; controller=VS.transposeController, solver=DE.TRBDF2, 
-    quiet::Bool=false, step_size::Union{Real,Nothing} = nothing, terminal_state::Union{Vector{T} where T<:Real, Nothing} = nothing, terminal_threshold::Real = 1e-6)
+    quiet::Bool=false, step_size::Union{Real,Nothing} = nothing, terminal_state::Union{Vector{T} where T<:Real, Vector{Vector{T}} where T<:Real, Nothing} = nothing, terminal_threshold::Real = 1e-6)
 
-    return simulate(f,x0,[P,sd,controller],t,solver=solver,quiet=quiet,step_size=step_size,terminal_state=terminal_state,terminal_threshold=terminal_threshold);
+    return simulate(f,x0,(P = P,sd = sd,controller = controller),t,solver=solver,quiet=quiet,step_size=step_size,terminal_state=terminal_state,terminal_threshold=terminal_threshold);
 end
 
-function singleSimulation(f::Function, x0::AbstractArray{<:Real}, p::Vector{Any}, t::Real, solver; step_size::Union{Real,Nothing} = nothing, 
-    terminal_state::Union{Vector{T} where T<:Real, Nothing} = nothing, terminal_threshold::Real = 1e-6)
+function singleSimulation(f::Function, x0::AbstractArray{<:Real}, p::NamedTuple, t::Real, solver; step_size::Union{Real,Nothing} = nothing, 
+    terminal_distance::Function = VS.squaredDistance, 
+    terminal_state::Union{Vector{T} where T<:Real, Vector{Vector{T}} where T<:Real, Nothing} = nothing, terminal_threshold::Real = 1e-6)
     
-    #p = [P, sd, controller];
     tol = 1e-8;
 
     prob = DE.ODEProblem(f,x0,(0.0, t),p);
 
     if isa(terminal_state,Nothing)
-        sol = isa(step_size,Nothing) ? DE.solve(prob,solver(),reltol=tol,abstol=tol) : DE.solve(prob,DE.RK4(),dt=step_size);
+        sol = isa(step_size,Nothing) ? DE.solve(prob,solver(),reltol=tol,abstol=tol) : DE.solve(prob,DE.Euler(),dt=step_size);
     else
-        condition(u,t,integrator) = VS.squaredDistance(f,u,terminal_state)-terminal_threshold^2;
-        
+        condition(u,t,integrator) = isa(terminal_state, Vector{Vector{T}} where T<:Real) ? minimum([(terminal_distance(f,u,t_s)-terminal_threshold^2) for t_s in terminal_state]) : terminal_distance(f,u,terminal_state)-terminal_threshold^2;
         affect!(integrator) = DE.terminate!(integrator);
         cb = DE.ContinuousCallback(condition,affect!);
-        sol = isa(step_size,Nothing) ? DE.solve(prob,solver(),callback=cb) : DE.solve(prob,DE.RK4(),callback=cb,dt = step_size);
+        sol = isa(step_size,Nothing) ? DE.solve(prob,solver(),callback=cb) : DE.solve(prob,DE.Euler(),callback=cb,dt = step_size);
 
         if sol.t[end] == t
             @warn "Warning: a simulation reached the final time without activating the termination condition.";
