d75299c0f6a0c1e7728c58d279bca4bf7e1cb15f
Alessandro <alessandro.colotti@inria.fr>
2024-11-14 15:44:23 +0100

    Added support for xyz Euler angles

--- src/RotationConversions/RotationConversions.jl
+++ src/RotationConversions/RotationConversions.jl
@@ -3,7 +3,38 @@ module RotationConversions
 import LinearAlgebra
 
 export angleAxisToRotationMatrix, quaternionToRotationMatrix, angleAxisToQuaternion,
-       rotationMatrixToQuaternion, quaternionToAngleAxis, rotationMatrixToAngleAxis
+       rotationMatrixToQuaternion, quaternionToAngleAxis, rotationMatrixToAngleAxis,
+       rotationMatrixToEulerAngles, EulerAnglesToRotationMatrix
+
+function rotationMatrixToEulerAngles(R::AbstractMatrix)
+    if abs(R[3,1]) - 1 <= 1e-30
+        θ₁ = asin(R[3,1]);
+        θ₂ = π - θ₁;
+
+        ψ₁ = atan((R[3,2:3]/cos(θ₁))...);
+        ϕ₁ = atan(([R[2,1],R[1,1]]/cos(θ₁))...);
+        ψ₂ = atan((R[3,2:3]/cos(θ₂))...);
+        ϕ₂ = atan(([R[2,1],R[1,1]]/cos(θ₂))...);
+
+        if any(abs.([ψ₁,θ₁,ϕ₁]) .> π)
+            return [ψ₂,θ₂,ϕ₂];
+        else
+            return [ψ₁,θ₁,ϕ₁];
+        end
+    else
+        ϕ = 0;
+        s = -sign(R[3,1]);
+        θ = s*π/2;
+        ψ = atan(s*R[1,2],s*R[1,3]);
+
+        return [ψ,θ,ϕ];
+    end
+end
+
+function EulerAnglesToRotationMatrix(A::AbstractVector)
+    a,b,c = A;
+    return [1 0 0; 0 cos(a) -sin(a); 0 sin(a) cos(a)] * [cos(b) 0 sin(b); 0 1 0; -sin(b) 0 cos(b)] * [cos(c) -sin(c) 0; sin(c) cos(c) 0; 0 0 1];
+end
 
 function angleAxisToRotationMatrix(S::AbstractVector)
     K(u) = [0 -u[3] u[2]; u[3] 0 -u[1]; -u[2] u[1] 0];
